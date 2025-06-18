7c36bd73cd854af0ebc35cd25d9cc77e59b6ea06
Limber <it@limberenergy.com>
2025-06-18 15:21:39 +0530

    basic php

---
+++ constant.php
@@ -0,0 +1,10 @@
+<?php
+const name="Shikha Singh";
+echo name;
+
+
+define("DATA", "Aman");
+echo DATA;x
+$name="Vikas Singh";
+echo $name;
+?>
---
+++ datatype.php
@@ -0,0 +1,16 @@
+<?php
+$name="Anil Sharma";
+echo var_dump($name);
+$age=19;
+echo var_dump($age);
+$number=18.5;
+echo var_dump($number);
+$boolean=true;
+echo var_dump($boolean);
+$arr=[1,2,3,4,"ss"];
+echo var_dump($arr);
+$empty=null;
+echo var_dump($empty);
+
+// single line comment
+/* multiple line */ 
